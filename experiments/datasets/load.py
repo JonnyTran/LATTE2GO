4863ea06087138713acd031b4e94a9f4c7c7598e
JonnyTran <JonnyTran@github.com>
2023-01-27 00:43:44 -0600

    MultiLabelBinarizer for DeepGraphGO/data/

--- experiments/datasets/load.py
+++ experiments/datasets/load.py
@@ -21,14 +21,12 @@ def load_node_dataset(name: str, method, hparams: Namespace,
         dataset = None  # will load in method
 
     elif "HUMAN_MOUSE" in name or "MULTISPECIES" in name:
-        extra_args = {}
         if name == 'HUMAN_MOUSE':
-            dataset_path = 'data/heteronetwork/DGG_HUMAN_MOUSE_MirTarBase_TarBase_LncBase_RNAInter_STRINGphyssplit_BioGRID_mRNAprotein_transcriptlevel.HeteroNetwork'
+            dataset_path = 'data/heteronetwork/UniProt.InterPro.HUMAN_MOUSE.DGG.parents'
         elif name == "MULTISPECIES":
-            dataset_path = 'data/heteronetwork/DGG_MirTarBase_TarBase_LncBase_RNAInter_STRINGphyssplit_BioGRID_mRNAprotein_transcriptlevel.HeteroNetwork'
+            dataset_path = 'data/heteronetwork/UniProt.InterPro.MULTISPECIES.DGG.parents'
 
-        mlb_path = os.path.expanduser('~/Bioinformatics_ExternalData/LATTE2GO')
-        mlb_paths = glob.glob(f'{mlb_path}/{hparams.dataset}-{hparams.pred_ntypes}/go_id.mlb')
+        mlb_paths = glob.glob(f'data/DeepGraphGO/data/{hparams.pred_ntypes}.mlb')
         if mlb_paths:
             hparams.mlb_path = mlb_paths[0]
 
@@ -38,7 +36,7 @@ def load_node_dataset(name: str, method, hparams: Namespace,
         if 'LATTE2GO' in hparams.method and hparams.pred_ntypes not in hparams.ntype_subset:
             hparams.ntype_subset = hparams.ntype_subset + ' ' + hparams.pred_ntypes
 
-        dataset = build_cafa_dataset('UniProt', dataset_path=dataset_path, hparams=hparams, **extra_args)
+        dataset = build_cafa_dataset('UniProt', dataset_path=dataset_path, hparams=hparams)
 
     else:
         raise Exception(f"dataset {name} not found")
