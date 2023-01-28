3c792583d4b34a717541d1ce4a12ac17b3837029
JonnyTran <JonnyTran@github.com>
2023-01-28 12:59:08 -0600

    latest

--- download_data.py
+++ download_data.py
@@ -74,7 +74,7 @@ def download_url_files(output_dir:str, baseurl:str, files:List[str]):
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--bucket_name", type=str, help="Name of the bucket to download from",
-                        default="arn:aws:s3:us-west-2:770865302968:accesspoint/latte2go-cafa-dataset-ap:latte2go-cafa-dataset")
+                        default="latte2go-cafa-datase-m7x88wj7izxopmw3buxncifp8x5uausw2b-s3alias:latte2go-cafa-dataset")
     parser.add_argument("--output_dir", type=str, help="Directory to download the files to", default="data/")
     args = parser.parse_args()
 
