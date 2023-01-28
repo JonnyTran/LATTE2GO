dbf6f0484143f7a50d5dd0ae6fbf24ce1d64c914
JonnyTran <JonnyTran@github.com>
2023-01-27 22:46:19 -0600

    latest fixes

--- experiments/datasets/load.py
+++ experiments/datasets/load.py
@@ -1,15 +1,7 @@
-import glob
-import logging
-import os
-import pickle
 from argparse import Namespace
-from pathlib import Path
-from typing import Union
 
-import pandas as pd
 from ruamel import yaml
 
-from moge.network.hetero import HeteroNetwork
 from experiments.datasets.CAFA import build_cafa_dataset
 
 
@@ -27,16 +19,6 @@ def load_node_dataset(name: str, method:str, hparams: Namespace,
             dataset_path = 'data/UniProt.InterPro.MULTISPECIES.DGG.HeteroNetwork/'
         else:
             raise Exception(f"Dataset name {name} not supported.")
-
-        if hparams.pred_ntypes == "biological_process":
-            hparams.mlb_path = f'data/DeepGraphGO/data/bp_go.mlb'
-        elif hparams.pred_ntypes == "molecular_function":
-            hparams.mlb_path = f'data/DeepGraphGO/data/mf_go.mlb'
-        elif hparams.pred_ntypes == "cellular_component":
-            hparams.mlb_path = f'data/DeepGraphGO/data/cc_go.mlb'
-        else:
-            # Prediction on classes of two or more ontologies
-            hparams.mlb_path = f'data/all_go.mlb'
 
         # Update additional params from `experiments/configs/latte2go.yaml`
         with open(latte2go_yaml, 'r') as f:
